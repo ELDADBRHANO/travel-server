ddf16002cf5e464c81926c33c6c8bebb788e5b63
ELDADBRHANO <ebrhano@gmail.com>
2022-11-18 15:16:38 +0200

    .

--- validation/age-validation.js
+++ validation/age-validation.js
@@ -1,7 +1,6 @@
 const getAge=(birthYear)=>{
   let year_born = birthYear.slice(-4);
   let date = new Date();
-  let n = date.getFullYear();
 	let currentDate = new Date();
     let currentYear = currentDate.getFullYear();
     age = currentYear - year_born;
